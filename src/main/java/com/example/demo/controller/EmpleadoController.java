51a82741837e7844a1ebc07cecb5617362b22a24
Esteban-CH <josestebanchinchayr@gmail.com>
2024-06-21 12:22:11 -0500

    CRUD CASI COMPLETO DEL EMPLEADO(Detalle falta terminar)

--- src/main/java/com/example/demo/controller/EmpleadoController.java
+++ src/main/java/com/example/demo/controller/EmpleadoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -36,6 +37,33 @@ public class EmpleadoController {
 		empleado.setFchaActualizacionEmpleado(new Date());
 		repository.save(empleado);
 		return("redirect:/listar_empleado");
+	}
+	
+	@GetMapping("/editar_empleado/{id}")
+	public String mostrarVistaEditarEmpleado(@PathVariable("id")Integer id, Model model) {
+		EmpleadoEntity empleado = repository.findById(id).get();
+		model.addAttribute("empleado", empleado);
+		return("/empleados/editar");
+	}
+	
+	@PostMapping("/editar_empleado/{id}")
+	public String editarEmpleado(@ModelAttribute EmpleadoEntity empleado, @PathVariable("id")Integer id, Model model) {
+		empleado.setEmpleadoId(id);
+		// Actualizar la contraseña solo si se proporciona una nueva
+        if (empleado.getPasswordEmpleado() != null && !empleado.getPasswordEmpleado().isEmpty()) {
+            empleado.setPasswordEmpleado(empleado.getPasswordEmpleado());
+        }
+		empleado.setFchaCreacionEmpleado(empleado.getFchaCreacionEmpleado());
+		empleado.setFchaActualizacionEmpleado(new Date());
+		repository.save(empleado);
+		return("redirect:/listar_empleado");
+	}
+	
+	@GetMapping("/detalle_empleado/{id}")
+	public String verEmpleado(Model model, @PathVariable("id")Integer id) {
+		EmpleadoEntity empleado = repository.findById(id).get();
+		model.addAttribute("empleado", empleado);
+		return ("/empleados/detalle");
 	}
 	
 	@GetMapping("eliminar_empleado/{id}")
